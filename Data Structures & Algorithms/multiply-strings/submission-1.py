2152006a3913154a40356f73a65d4d3d8559048b
Olric Zeng <59369019+frxyoz@users.noreply.github.com>
2026-07-31 20:59:31 -0700

    Add: multiply-strings - submission-1

---
+++ Data Structures & Algorithms/multiply-strings/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if "0" in [num1, num2]:
+            return "0"
+        l1, l2 = len(num1), len(num2)
+        res = [0] * (l1 + l2)
+
+        num1, num2 = num1[::-1], num2[::-1]
+        for i in range(l1):
+            for j in range(l2):
+                summation = int(num1[i]) * int(num2[j])
+                res[i+j] += summation
+                res[i + j + 1] += res[i+j] // 10
+                res[i+j] = res[i+j] % 10
+        res = res[::-1]
+        start = 0 
+        while start < len(res) and res[start] == 0: 
+            start += 1 
+        res = map(str,res[start:])
+        return "".join(res)
+    
+        
